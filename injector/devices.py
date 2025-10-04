a72df6642c458c27936ef18a91231bc3b326318f
abdulla-lin <abdullashakkeeb4@gmail.com>
2025-10-04 19:25:28 +0530

    Update devices.py

--- injector/devices.py
+++ injector/devices.py
@@ -143,14 +143,7 @@ DEVICES = [
                 replacement='fd 7b be a9 f3 0b 00 f9 fd 03 00 91 f3 03 00 aa 48 04 80 52 68 02 00 b9 e0 03 1f 2a f3 0b 40 f9 fd 7b c2 a8',
                 match_mode=MatchMode.ALL,
                 description='Force sboot state to always be ATTR_SBOOT_ONLY_ENABLE_ON_SCHIP',
-            ),
-            'spoof_lock_state': PatchStage(
-                'spoof_lock_state',
-                pattern='20 02 00 b4 fd 7b be a9 f3 0b 00 f9 fd 03 00 91',
-                replacement='88 00 80 52 08 00 00 b9 00 00 80 52 c0 03 5f d6',
-                match_mode=MatchMode.ALL,
-                description='Force lock state to always be LKS_LOCK',
-            )
+            )  
         },
         base=0xFFFF000050700000
     ),
