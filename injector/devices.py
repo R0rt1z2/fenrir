d1d18f16a099fdeadcd1eb9a168ad44ad4d530f0
abdulla-lin <abdullashakkeeb4@gmail.com>
2025-10-04 22:05:31 +0530

    Update devices.py

--- injector/devices.py
+++ injector/devices.py
@@ -137,41 +137,46 @@ DEVICES = [
         base=0xFFFF000050F00000,
     ),
     Device(
-        'Tetris',
-        'CMF Phone 1',
-        {
-            'sec_get_vfy_policy': PatchStage(
-                'sec_get_vfy_policy',
-                pattern='00 01 00 b4 fd 7b bf a9',
-                replacement='00 00 80 52 c0 03 5f d6',
-                match_mode=MatchMode.ALL,
-                description='Don\'t enforce secure boot policy',
-            ),
-            'force_green_state': PatchStage(
-                'force_green_state',
-                pattern='68 04 00 f0 00 d9 04 b9 c0 03 5f d6',
-                replacement='68 04 00 f0 1f d9 04 b9 c0 03 5f d6',
-                match_mode=MatchMode.ALL,
-                description='Force boot state to always be set to green',
-            ),
-            'spoof_sboot_state': PatchStage(
-                'spoof_sboot_state',
-                pattern='fd 7b be a9 f3 0b 00 f9 fd 03 00 91 f3 03 00 aa 20 00 80 52 c4 ff ff 97 e8 03 00 2a e0 03 1f 2a 68 02 00 b9',
-                replacement='fd 7b be a9 f3 0b 00 f9 fd 03 00 91 f3 03 00 aa 48 04 80 52 68 02 00 b9 e0 03 1f 2a f3 0b 40 f9 fd 7b c2 a8',
-                match_mode=MatchMode.ALL,
-                description='Force sboot state to always be ATTR_SBOOT_ONLY_ENABLE_ON_SCHIP',
-            ),
-            'spoof_lock_state': PatchStage(
-                'spoof_lock_state',
-                pattern='20 02 00 b4 fd 7b be a9 f3 0b 00 f9 fd 03 00 91',
-                replacement='88 00 80 52 08 00 00 b9 00 00 80 52 c0 03 5f d6',
-                match_mode=MatchMode.ALL,
-                description='Force lock state to always be LKS_LOCK',
-            ),
-            'bypass_security_control': PatchStage(
+    'Tetris',
+    'CMF Phone 1',
+    {
+        # Disable secure boot policy
+        'sec_get_vfy_policy': PatchStage(
+            'sec_get_vfy_policy',
+            pattern='00 01 00 b4 fd 7b bf a9',
+            replacement='00 00 80 52 c0 03 5f d6',
+            match_mode=MatchMode.ALL,
+            description="Don't enforce secure boot policy",
+        ),
+        # Force boot state to always be green
+        'force_green_state': PatchStage(
+            'force_green_state',
+            pattern='68 04 00 f0 00 d9 04 b9 c0 03 5f d6',
+            replacement='68 04 00 f0 1f d9 04 b9 c0 03 5f d6',
+            match_mode=MatchMode.ALL,
+            description='Force boot state to always be set to green',
+        ),
+        # Spoof secure boot state
+        'spoof_sboot_state': PatchStage(
+            'spoof_sboot_state',
+            pattern='fd 7b be a9 f3 0b 00 f9 fd 03 00 91 f3 03 00 aa 20 00 80 52 c4 ff ff 97 e8 03 00 2a e0 03 1f 2a 68 02 00 b9',
+            replacement='fd 7b be a9 f3 0b 00 f9 fd 03 00 91 f3 03 00 aa 48 04 80 52 68 02 00 b9 e0 03 1f 2a f3 0b 40 f9 fd 7b c2 a8',
+            match_mode=MatchMode.ALL,
+            description='Force sboot state to always be ATTR_SBOOT_ONLY_ENABLE_ON_SCHIP',
+        ),
+        # Spoof lock state
+        'spoof_lock_state': PatchStage(
+            'spoof_lock_state',
+            pattern='20 02 00 b4 fd 7b be a9 f3 0b 00 f9 fd 03 00 91',
+            replacement='88 00 80 52 08 00 00 b9 00 00 80 52 c0 03 5f d6',
+            match_mode=MatchMode.ALL,
+            description='Force lock state to always be LKS_LOCK',
+        ),
+        # Bypass critical security error branch (FIX BOOTLOOP!)
+        'bypass_security_control': PatchStage(
             'bypass_security_control',
             pattern='24 74 01 94 20 01 00 36',
-            replacement='24 74 01 94 1f 20 03 d5',  # NOP instead of branch
+            replacement='24 74 01 94 1f 20 03 d5',  # NOP to always skip error
             match_mode=MatchMode.ALL,
             description='Skip security error branch - always execute commands',
         ),
